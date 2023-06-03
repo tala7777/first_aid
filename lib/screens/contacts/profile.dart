d34d32877f985826663305f614f2a38b855b1b3d
tala7777 <125188487+tala7777@users.noreply.github.com>
2023-06-03 16:45:21 +0300

    fixed mistakes2

--- lib/screens/contacts/profile.dart
+++ lib/screens/contacts/profile.dart
@@ -33,77 +33,79 @@ class _ProfileState extends State<Profile> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
       ),
-      body:  Column(children: [
-        Container(
-        height: deviceheight / 2.2,
-        width: devicewidth,
-        color: const Color(0xFFF5533D),
-
+      body:  SingleChildScrollView(
         child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-            child: CircleAvatar(
-              backgroundImage: const AssetImage(
-                  'lib/assets/images/img_2.png'),
-              radius: devicewidth / 7,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-            child: Text('911',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: Colors.white)),
-
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-            child: Text('Number 911',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w100,
-                    fontSize: 15,
-                    color: Colors.white)),
-
-          ),
           Container(
-              padding: const EdgeInsets.all(8.0),
-              child:  IconButton(color: Colors.white, onPressed: () {  }, icon: const Icon(Icons.phone,size: 40,),
+          height: deviceheight / 2,
+          width: devicewidth,
+          color: const Color(0xFFF5533D),
+
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: CircleAvatar(
+                backgroundImage: const AssetImage(
+                    'lib/assets/images/img_2.png'),
+                radius: devicewidth / 7,
               ),
             ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+              child: Text('911',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.white)),
+
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+              child: Text('Number 911',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w100,
+                      fontSize: 15,
+                      color: Colors.white)),
+
+            ),
+            Container(
+                padding: const EdgeInsets.all(8.0),
+                child:  IconButton(color: Colors.white, onPressed: () {  }, icon: const Icon(Icons.phone,size: 40,),
+                ),
+              ),
+
+          ]),
+        ),
+           Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+              alignment: Alignment.bottomLeft,
+            child: const Text('Address: None ',
+                textAlign: TextAlign.left,
+
+                style: TextStyle(
+            fontFamily: 'Italiana',
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Color(0xFFB83434))),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+            alignment: Alignment.bottomLeft,
+            child: const Text('Availability: 24/7 ',
+                textAlign: TextAlign.left,
+
+                style: TextStyle(
+                    fontFamily: 'Italiana',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color(0xFFB83434))),
+          ),
 
         ]),
       ),
-         Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-            alignment: Alignment.bottomLeft,
-          child: const Text('Address: None ',
-              textAlign: TextAlign.left,
-
-              style: TextStyle(
-          fontFamily: 'Italiana',
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Color(0xFFB83434))),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          alignment: Alignment.bottomLeft,
-          child: const Text('Availability: 24/7 ',
-              textAlign: TextAlign.left,
-
-              style: TextStyle(
-                  fontFamily: 'Italiana',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color(0xFFB83434))),
-        ),
-
-      ]),
     );
   }
 }
--- lib/screens/contacts/profile2.dart
+++ lib/screens/contacts/profile2.dart
@@ -34,77 +34,79 @@ class _Profile2State extends State<Profile2> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
       ),
-      body:  Column(children: [
-        Container(
-          height: deviceheight / 2.2,
-          width: devicewidth,
-          color: const Color(0xFFF5533D),
+      body:  SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            height: deviceheight / 2.2,
+            width: devicewidth,
+            color: const Color(0xFFF5533D),
 
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-              child: CircleAvatar(
-                backgroundImage: const AssetImage(
-                    'lib/assets/images/img_1.png'),
-                radius: devicewidth / 7,
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: CircleAvatar(
+                  backgroundImage: const AssetImage(
+                      'lib/assets/images/img_1.png'),
+                  radius: devicewidth / 7,
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
-              child: Text('The specialty hospital',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28,
-                      color: Colors.white)),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                child: Text('The specialty hospital',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                        color: Colors.white)),
 
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-              child: Text('Number 06-200-1111',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w100,
-                      fontSize: 15,
-                      color: Colors.white)),
-
-            ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              child:  IconButton(color: Colors.white, onPressed: () {  }, icon: const Icon(Icons.phone,size: 40,),
               ),
-            ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Text('Number 06-200-1111',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w100,
+                        fontSize: 15,
+                        color: Colors.white)),
 
-          ]),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          alignment: Alignment.bottomLeft,
-          child: const Text('Address: Jordan, Amman, al-shmesani ',
-              textAlign: TextAlign.left,
+              ),
+              Container(
+                padding: const EdgeInsets.all(0.0),
+                child:  IconButton(color: Colors.white, onPressed: () {  }, icon: const Icon(Icons.phone,size: 40,),
+                ),
+              ),
 
-              style: TextStyle(
-                  fontFamily: 'Italiana',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color(0xFFB83434))),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          alignment: Alignment.bottomLeft,
-          child: const Text('Availability: 24/7 ',
-              textAlign: TextAlign.left,
+            ]),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+            alignment: Alignment.bottomLeft,
+            child: const Text('Address: Jordan, Amman, al-shmesani ',
+                textAlign: TextAlign.left,
 
-              style: TextStyle(
-                  fontFamily: 'Italiana',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Color(0xFFB83434))),
-        ),
+                style: TextStyle(
+                    fontFamily: 'Italiana',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color(0xFFB83434))),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
+            alignment: Alignment.bottomLeft,
+            child: const Text('Availability: 24/7 ',
+                textAlign: TextAlign.left,
 
-      ]),
+                style: TextStyle(
+                    fontFamily: 'Italiana',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color(0xFFB83434))),
+          ),
+
+        ]),
+      ),
     );
   }
 }
